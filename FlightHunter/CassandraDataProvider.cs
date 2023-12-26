f27b61195b02f68c61c0a2c97424bc6fab52e583
Luka <Luka@LUKA>
2023-12-26 18:22:32 +0100

    Add project files.

---
+++ FlightHunter/CassandraDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using Cassandra;
+using CassandraDataLayer.QueryEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CassandraDataLayer
+{
+    public static class CassandraDataProvider
+    {
+
+    }
+}
---
+++ FlightHunter/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FlightHunter.DomainModel;
+using Neo4jClient;
+using Neo4jClient.Cypher;
+using CassandraDataLayer;
+using CassandraDataLayer.QueryEntities;
+
+namespace FlightHunter
+{
+    public partial class Form1 : Form
+    {
+        private GraphClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new GraphClient(new Uri("http://localhost:7474/db/data/"), "neo4j", "neo4jneo4j");
+            try
+            {
+                client.Connect();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
